969078ccb1a96d7264d8641a44d558f4f90c5fb1
Paul MacLachlan <paulmaclachlan@PM15.local>
2018-08-06 17:44:38 -0700

    Adds login functionality

--- app.py
+++ app.py
@@ -26,6 +26,17 @@ def index():
         return redirect(url_for('index'))
     return render_template('index.html', audits=audits)
 
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
